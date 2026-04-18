20e62e30cfbc20ec90057709cc1581cbf46881c1
Brian Searls <briansrls@gunb.ai>
2026-04-18 13:34:01 -0400

    WIP: ζ

--- src/v3/compiler/tests/test_recvar.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn compile_record_variant_body() {
-    let source = std::fs::read_to_string("/tmp/test_record_variant.dag").unwrap();
-    let dag = v3_compiler::compile_to_dag(&source, "test.v3").expect("compiles");
-    for (p, d) in dag.diagnostics().iter() {
-        println!("{:?}: {:?}", p, d);
-    }
-    assert!(dag.diagnostics().is_empty());
-}
