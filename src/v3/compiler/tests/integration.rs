2d128885ed2776045745d927456d974d81c5b89d
Brian Searls <briansrls@gunb.ai>
2026-04-22 13:40:46 -0400

    WIP: C

--- src/v3/compiler/tests/integration.rs
+++ src/v3/compiler/tests/integration.rs
@@ -97,6 +97,8 @@ mod m2_lens_structural_resolution_migration_test;
 mod m2_lens_unused_parameters_migration_test;
 #[path = "integration/m2_lens_variant_payload_migration_test.rs"]
 mod m2_lens_variant_payload_migration_test;
+#[path = "integration/lens_register_correspondence_test.rs"]
+mod lens_register_correspondence_test;
 #[path = "integration/m2_substrate_inhabitance_test.rs"]
 mod m2_substrate_inhabitance_test;
 #[path = "integration/p0_std_render_repeat_string_test.rs"]
