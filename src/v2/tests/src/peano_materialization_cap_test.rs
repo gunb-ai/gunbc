0603a137fd397f17e1e7ac2cbb861d4a057109bc
Brian Searls <briansrls@gunb.ai>
2026-04-24 15:42:46 -0400

    WIP: E-I

--- src/v2/tests/src/peano_materialization_cap_test.rs
+++ src/v2/tests/src/peano_materialization_cap_test.rs
@@ -38,6 +38,15 @@ fn sum_bound_has_cost_bound_introspection_consumer() {
 }
 
 #[test]
+fn sum_bound_rejects_empty_alternative_stack() {
+    let terms = Rc::new(vec![]);
+    assert!(matches!(
+        sum_bound(terms).as_ref(),
+        CostBound::ErrorBound
+    ));
+}
+
+#[test]
 fn master_theorem_rejects_work_exponent_above_peano_cap() {
     let form = Rc::new(RecurrenceForm {
         param: "n".to_string(),
