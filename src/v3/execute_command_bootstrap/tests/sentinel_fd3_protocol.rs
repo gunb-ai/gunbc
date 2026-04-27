e0434742d30b4b7f4619b045182b4467d0796cf5
Brian Searls <briansrls@gunb.ai>
2026-04-27 15:31:49 -0400

    WIP: gentle-badger-838

--- src/v3/execute_command_bootstrap/tests/sentinel_fd3_protocol.rs
+++ src/v3/execute_command_bootstrap/tests/sentinel_fd3_protocol.rs
@@ -5,6 +5,7 @@
 //! child before exec.
 
 use std::io::Read;
+use std::os::fd::FromRawFd;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 
@@ -16,12 +17,7 @@ fn pipe() -> (i32, i32) {
     (fds[0], fds[1])
 }
 
-fn spawn_bootstrap_with_sentinel_fd(
-    bin: &str,
-    read_fd: i32,
-    write_fd: i32,
-    args: &[&str],
-) -> std::process::Child {
+fn spawn_bootstrap_with_sentinel_fd(bin: &str, read_fd: i32, write_fd: i32, args: &[&str]) -> std::process::Child {
     let mut cmd = Command::new(bin);
     for a in args {
         cmd.arg(a);
