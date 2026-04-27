a194cb19ca905a7aea9128bed54b210c2979ccf2
Brian Searls <briansrls@gunb.ai>
2026-04-27 11:59:27 -0400

    chore: apply cargo fmt

--- src/v3/compiler/tests/integration.rs
+++ src/v3/compiler/tests/integration.rs
@@ -297,8 +297,8 @@ mod lane2_stage_2f_dimension_test {
     use v3_compiler::analyze_symbolic_cost_dimension;
     use v3_compiler::compile_to_dag;
     use v3_compiler::dag::{Behavior, Dag, DeclarationId, PortId, TypeConnective};
-    use v3_compiler::DimensionReport;
     use v3_compiler::lens_cost_symbolic::{symbolic_cost_of, SymbolicCostLookup};
+    use v3_compiler::DimensionReport;
 
     fn find_bind_port(dag: &Dag, name: &str) -> PortId {
         dag.nodes()
