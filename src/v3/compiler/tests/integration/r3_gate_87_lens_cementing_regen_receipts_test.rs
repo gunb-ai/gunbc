ceecdc03779cd52618d05838e0bbd94d4cd7a056
Brian Searls <briansrls@gmail.com>
2026-05-13 06:48:25 -0400

    WIP: G87-D1 registry-invariant audit: align regen registry, runner table, per

--- src/v3/compiler/tests/integration/r3_gate_87_lens_cementing_regen_receipts_test.rs
+++ src/v3/compiler/tests/integration/r3_gate_87_lens_cementing_regen_receipts_test.rs
@@ -41,14 +41,16 @@ use std::path::PathBuf;
 use v3_compiler::r3_gate_87_cementing_regen_runner_suites::r3_gate_87_cementing_regen_lens_names_for_runner_table;
 
 use v3_compiler::compile_to_dag;
-use v3_compiler::dag::{Behavior, Declaration, FieldValue, LiteralBits, ValueBody};
+use v3_compiler::dag::{
+    Behavior, Declaration, FieldValue, LiteralBits, ValueBody, WorkflowParallelismReport,
+};
 use v3_compiler::lens_cost_target_realization::type_realization_meta;
 use v3_compiler::lens_effect_enumeration::{enumerate_effects, TransactionalPattern};
 use v3_compiler::lens_parallelism::analyze_parallelism;
 use v3_compiler::lens_provenance::{origin_of, Origin};
 use v3_compiler::lens_structural_resolution;
 use v3_compiler::lens_unused_parameters::{UnusedParametersConfig, UnusedParametersLens};
-use v3_compiler::{Dag, WorkflowParallelismReport};
+use v3_compiler::Dag;
 
 fn workspace_root() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
@@ -204,8 +206,8 @@ fn r3_gate_87_lower_helpers_lens_source_compiles() {
 
 #[test]
 fn r3_gate_87_parallelism_rust_receipt_on_literal_program() {
-    let dag = compile_to_dag("let lit: Int = 7", "r3_gate_87_parallelism_receipt.v3")
-        .expect("compile");
+    let dag =
+        compile_to_dag("let lit: Int = 7", "r3_gate_87_parallelism_receipt.v3").expect("compile");
     let root = dag
         .nodes()
         .first()
