3af0bbf9695943748a55255028ca03b35ccc3437
Brian Searls <briansrls@gmail.com>
2026-05-13 02:28:36 -0400

    WIP: G87-D2 complexity/cost report-predicate carrier cementing

--- src/v3/compiler/src/r3_gate_87_cementing_regen_runner_suites.rs
+++ src/v3/compiler/src/r3_gate_87_cementing_regen_runner_suites.rs
@@ -88,6 +88,12 @@ pub const R3_GATE_87_CEMENTING_REGEN_SUITES: &[(&str, &str, &str, &[&str])] = &[
         &["cementing_regen_effect_enumeration"],
     ),
     (
+        include_str!("../tests/dag/t_r3_gate_87_cementing_regen_parallelism.dag"),
+        "src/v3/compiler/tests/dag/t_r3_gate_87_cementing_regen_parallelism.dag",
+        "r3_gate_87_cementing_regen_parallelism_suite",
+        &["cementing_regen_parallelism"],
+    ),
+    (
         include_str!("../tests/dag/t_r3_gate_87_cementing_regen_infer_helpers.dag"),
         "src/v3/compiler/tests/dag/t_r3_gate_87_cementing_regen_infer_helpers.dag",
         "r3_gate_87_cementing_regen_infer_helpers_suite",
