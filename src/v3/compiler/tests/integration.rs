b1634c4214ff77633234ab3b922282c2e663fc6b
Brian Searls <briansrls@gmail.com>
2026-05-14 17:00:27 +0000

    fmt: alphabetize symbolic_cost_expr_equals_executable_ratchet_test module wiring

--- src/v3/compiler/tests/integration.rs
+++ src/v3/compiler/tests/integration.rs
@@ -159,8 +159,6 @@ mod m2_substrate_inhabitance_test;
 mod memory_peak_cost_basis_demo;
 #[path = "integration/method_registry_test.rs"]
 mod method_registry_test;
-#[path = "integration/symbolic_cost_expr_equals_executable_ratchet_test.rs"]
-mod symbolic_cost_expr_equals_executable_ratchet_test;
 #[path = "integration/method_template_contract_test.rs"]
 mod method_template_contract_test;
 #[path = "integration/method_template_projection_emit_shim_coherence_test.rs"]
@@ -221,6 +219,8 @@ mod sg6_hand_authored_census_test;
 mod sg7_prep_variant_payload_freshness_test;
 #[path = "integration/shape_a_target_source_filtering_authority_test.rs"]
 mod shape_a_target_source_filtering_authority_test;
+#[path = "integration/symbolic_cost_expr_equals_executable_ratchet_test.rs"]
+mod symbolic_cost_expr_equals_executable_ratchet_test;
 #[path = "integration/t_ci_workflow_as_data_demo_test.rs"]
 mod t_ci_workflow_as_data_demo_test;
 #[path = "integration/t_gate_58_apply_lens_self_application_test.rs"]
