7bb4b79d9b5ab2cc9c34f4579710175aa1fca634
Brian Searls <briansrls@gmail.com>
2026-05-14 13:23:56 -0400

    WIP: R3 gate #76: e_p_per_call_descent_evidence_full_coverage (T-E-P-Producer

--- src/v3/compiler/tests/integration.rs
+++ src/v3/compiler/tests/integration.rs
@@ -59,6 +59,8 @@ mod cementing_provenance_origin_integration_test;
 mod complexity_lens_behavioral_completion;
 #[path = "integration/cementing/cost_lens_symbolic_consumer_test.rs"]
 mod cost_lens_symbolic_consumer_test;
+#[path = "integration/cementing/e_p_per_call_descent_lens_consumer_cementing.rs"]
+mod e_p_per_call_descent_lens_consumer_cementing;
 #[path = "integration/cross_target_coverage_carrier_test.rs"]
 mod cross_target_coverage_carrier_test;
 #[path = "integration/ctrl_pr_digests_dag_smoke_test.rs"]
