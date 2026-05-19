a3c7c879052c050ec62ca25553ba0aab4264c488
Brian Searls <briansrls@gmail.com>
2026-05-19 02:32:53 +0000

    Format v4 integration registrations

--- src/v3/compiler/tests/integration.rs
+++ src/v3/compiler/tests/integration.rs
@@ -273,10 +273,10 @@ mod thesis_validation_test;
 mod timing_lens_substrate_carrier_test;
 #[path = "integration/v2_oracle_no_remaining_test_consumers_test.rs"]
 mod v2_oracle_no_remaining_test_consumers_test;
-#[path = "integration/v4_test_bootstrap_infra_closeout_test.rs"]
-mod v4_test_bootstrap_infra_closeout_test;
 #[path = "integration/v4_lens_registry_dag_smoke_test.rs"]
 mod v4_lens_registry_dag_smoke_test;
+#[path = "integration/v4_test_bootstrap_infra_closeout_test.rs"]
+mod v4_test_bootstrap_infra_closeout_test;
 #[path = "integration/value_body_substrate_mirror_isomorphism_test.rs"]
 mod value_body_substrate_mirror_isomorphism_test;
 #[path = "integration/common/wiring_scanner_test.rs"]
