f6e2d8ce3dc5fa930eefbdd6fccae7b3a0fc2666
Brian Searls <briansrls@gmail.com>
2026-05-21 04:36:46 -0400

    WIP: BoundedLattice<T> inhabitance for Encoding / DescentEvidence / FermiDept

--- src/v2/stage0/src/std_termination.rs
+++ src/v2/stage0/src/std_termination.rs
@@ -61,14 +61,14 @@ pub fn descent_evidence_lattice_join(a: DescentEvidence, b: DescentEvidence) -> 
     }
 }
 
-pub fn descent_evidence_bounded_lattice() -> Rc<BoundedLattice> {
+pub fn descent_evidence_bounded_lattice() -> Rc<BoundedLattice<DescentEvidence>> {
     thread_local! {
-            static CACHED: Rc<BoundedLattice> = {
+            static CACHED: Rc<BoundedLattice<DescentEvidence>> = {
                 Rc::new(BoundedLattice {
         meet: Rc::new(descent_evidence_lattice_meet),
         join: Rc::new(descent_evidence_lattice_join),
-        top: DescentEvidence::Strict,
-        bottom: DescentEvidence::DescentUnknown,
+        top: Box::new(DescentEvidence::Strict),
+        bottom: Box::new(DescentEvidence::DescentUnknown),
     })
             };
         }
