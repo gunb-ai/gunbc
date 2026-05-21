f97e2899660b149f53549d4cecdd54bebc36da15
Brian Searls <briansrls@gmail.com>
2026-05-21 03:48:55 -0400

    WIP: BoundedLattice<T> inhabitance for Encoding / DescentEvidence / FermiDept

--- src/v2/stage0/src/v2_compiler_emit_rust.rs
+++ src/v2/stage0/src/v2_compiler_emit_rust.rs
@@ -10174,7 +10174,7 @@ pub fn rust_struct_field_type_node(
             field_name,
             scope.type_env.clone().source_indices.clone(),
         ) {
-            Some(field_node) => Some(resolved_type(field_node.clone())),
+            Some(field_node) => Some(child_type_node(&field_node_type_expr(&field_node))),
             None => None,
         },
         None => None,
