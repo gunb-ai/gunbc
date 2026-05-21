f7d30f68e1956805fc75577baa8f619491604fcc
Brian Searls <briansrls@gmail.com>
2026-05-21 11:53:59 -0400

    WIP: v2 stage0 substrate-fill — Set/BTreeSet migration to make regen_stage0 -

--- src/v2/stage0/src/v2_compiler_infer.rs
+++ src/v2/stage0/src/v2_compiler_infer.rs
@@ -10461,10 +10461,13 @@ pub fn build_type_env(
             cycle_set_str.clone(),
             Rc::new(v2_rt::map_keys(&compiler_recursive_types())),
         );
-        let cross_type_set_str = cross_type_all_names.clone().iter().cloned().fold(
-            v2_rt::rc_empty_set::<String>(),
-            |acc: _, name: String| v2_rt::rc_set_insert(acc, name.clone()),
-        );
+        let cross_type_set_str = cross_type_all_names
+            .clone()
+            .iter()
+            .cloned()
+            .fold(v2_rt::rc_empty_set::<String>(), |acc: _, name: String| {
+                v2_rt::rc_set_insert(acc, name.clone())
+            });
         let cross_type_set = cross_type_all_names.clone().iter().cloned().fold(
             v2_rt::rc_empty_map::<i64, bool>(),
             |acc: Rc<HashMap<i64, bool>>, name: String| {
@@ -10920,10 +10923,13 @@ pub fn build_type_env_unresolved(
             cycle_set_str.clone(),
             Rc::new(v2_rt::map_keys(&compiler_recursive_types())),
         );
-        let cross_type_set_str = cross_type_all_names.clone().iter().cloned().fold(
-            v2_rt::rc_empty_set::<String>(),
-            |acc: _, name: String| v2_rt::rc_set_insert(acc, name.clone()),
-        );
+        let cross_type_set_str = cross_type_all_names
+            .clone()
+            .iter()
+            .cloned()
+            .fold(v2_rt::rc_empty_set::<String>(), |acc: _, name: String| {
+                v2_rt::rc_set_insert(acc, name.clone())
+            });
         let cross_type_set = cross_type_all_names.clone().iter().cloned().fold(
             v2_rt::rc_empty_map::<i64, bool>(),
             |acc: Rc<HashMap<i64, bool>>, name: String| {
@@ -11571,10 +11577,13 @@ pub fn topo_resolve_types(
                 diagnostics: diagnostics.clone(),
             });
         }
-        let remaining_set = remaining.clone().iter().cloned().fold(
-            v2_rt::rc_empty_set::<String>(),
-            |acc: _, name: String| v2_rt::rc_set_insert(acc, name.clone()),
-        );
+        let remaining_set = remaining
+            .clone()
+            .iter()
+            .cloned()
+            .fold(v2_rt::rc_empty_set::<String>(), |acc: _, name: String| {
+                v2_rt::rc_set_insert(acc, name.clone())
+            });
         let ready = Rc::new({
             let mut __result = Vec::new();
             for name in remaining.clone().iter().cloned() {
