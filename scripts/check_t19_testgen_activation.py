fd6bc1d99a35c71e547a3313fe66fefd689c1d22
Brian Searls <briansrls@gmail.com>
2026-05-22 02:25:58 -0400

    WIP: T-19 generator category: coproduct exhaustiveness TestClaim generation

--- scripts/check_t19_testgen_activation.py
+++ scripts/check_t19_testgen_activation.py
@@ -78,7 +78,7 @@ def main() -> None:
             "coproduct_exhaustiveness_subject_testclaim_equals",
             "coproduct_exhaustiveness_subject_testclaim_roundtrip",
             "t19_coproduct_exhaustiveness_missing_variant",
-            "omitted_variant: test_claim_coproduct_variant_symbol(variant: subject.omitted_variant)",
+            "omitted_variant: subject.omitted_variant",
             "t19_coproduct_exhaustiveness_omitted_variant_edge",
             "node_locus(node: input)",
         ),
@@ -91,7 +91,8 @@ def main() -> None:
             "T19ManualLbeConjDagSurface",
             "T19ManualLbeDisjDagSurface",
             "T19ManualLbeTransformDagSurface",
-            "T19GeneratedCoproductExhaustiveness { omitted_variant: Symbol }",
+            "type TestClaimCoproductVariant",
+            "T19GeneratedCoproductExhaustiveness { omitted_variant: TestClaimCoproductVariant }",
         ),
     )
 
