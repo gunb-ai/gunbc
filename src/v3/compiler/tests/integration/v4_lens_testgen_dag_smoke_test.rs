d2f489c03458bb2cfc560009b6319e5158034f3a
Brian Searls <briansrls@gmail.com>
2026-05-22 23:09:29 +0000

    WIP: t19

--- src/v3/compiler/tests/integration/v4_lens_testgen_dag_smoke_test.rs
+++ src/v3/compiler/tests/integration/v4_lens_testgen_dag_smoke_test.rs
@@ -199,7 +199,7 @@ fn v4_lens_testgen_wave0_outcome_return_surfaces() {
 }
 
 #[test]
-fn v4_lens_testgen_wave0_generator_t19_anchor_field_is_manual_key() {
+fn v4_lens_testgen_wave0_generator_t19_anchor_field_is_claim_anchor_key() {
     let testgen = parse_module(TESTGEN_DAG, "src/v4/lens/testgen.dag");
     let anchor_ty =
         generator_t19_anchor_field_ty(&testgen).expect("Generator should declare `t19_anchor`");
