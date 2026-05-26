39b272010f731e0568b102eecd94f71fe0805cd8
Brian Searls <briansrls@gunb.ai>
2026-05-25 22:59:57 -0400

    chore: apply cargo fmt

--- src/v2/stage0/src/v2_compiler_infer_resolve.rs
+++ src/v2/stage0/src/v2_compiler_infer_resolve.rs
@@ -672,9 +672,7 @@ pub fn resolve_node_bounded(
                     }
                 }
             }
-        } else if (((n.children.clone().len() as i64) == 0)
-            && is_user_generic_use_site(&n, &env))
-        {
+        } else if (((n.children.clone().len() as i64) == 0) && is_user_generic_use_site(&n, &env)) {
             {
                 let type_name = authored_name(env.clone(), n.clone());
                 let decl = match lookup_type_for(&env, &n) {
