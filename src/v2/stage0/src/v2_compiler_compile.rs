ef583b55d5fb30c85ac797c6e3e98c23537b9703
Brian Searls <briansrls@gmail.com>
2026-05-28 07:12:44 +0000

    WIP: T-37 fix: resolve dag_node_key span-based deduplication OOM in src/v2/co

--- src/v2/stage0/src/v2_compiler_compile.rs
+++ src/v2/stage0/src/v2_compiler_compile.rs
@@ -356,22 +356,20 @@ pub fn dag_node_key(mut node: Rc<Node>) -> String {
 }
 
 pub fn dag_node_provisional_key(node: Rc<Node>) -> String {
-    v2_rt::concat(
-        v2_rt::concat(
+    {
+        let fp = dag_node_fingerprint(node.clone());
+        if ((node.span.clone().start.clone() == 0) && (node.span.clone().end.clone() == 0)) {
             v2_rt::concat(
-                v2_rt::concat(
-                    v2_rt::concat(dag_node_fingerprint(node.clone()), "@".to_string()),
-                    (node.span.clone().start.clone()).to_string(),
-                ),
-                "..".to_string(),
-            ),
-            (node.span.clone().end.clone()).to_string(),
-        ),
-        match node.ident.clone() {
-            Some(id) => v2_rt::concat(":".to_string(), (id.clone()).to_string()),
-            None => "".to_string(),
-        },
-    )
+                v2_rt::concat(fp, "@0..0".to_string()),
+                match node.ident.clone() {
+                    Some(id) => v2_rt::concat(":".to_string(), (id.clone()).to_string()),
+                    None => "".to_string(),
+                },
+            )
+        } else {
+            fp
+        }
+    }
 }
 
 pub fn inferred_fingerprint(value: Option<Rc<InferredNode>>) -> String {
@@ -717,31 +715,11 @@ pub fn dag_collect_node_tree(node: Rc<Node>, acc: Rc<DagCollectAcc>) -> Rc<DagCo
 pub fn dag_collect_insert(node: Rc<Node>, acc: Rc<DagCollectAcc>) -> Rc<DagCollectAcc> {
     {
         let key = dag_node_key(node.clone());
-        let fp = dag_node_fingerprint(node.clone());
         match v2_rt::map_get(&acc.seen.clone(), key.clone()) {
-            Some(prior) => {
-                if (prior.clone().as_str() == fp.as_str()) {
-                    acc.clone()
-                } else {
-                    if ((node.span.clone().start.clone() == 0)
-                        && (node.span.clone().end.clone() == 0))
-                    {
-                        Rc::new(DagCollectAcc {
-                            seen: acc.seen.clone(),
-                            order: acc.order.clone(),
-                            collision_errors: v2_rt::rc_list_push(
-                                acc.collision_errors.clone(),
-                                dag_node_key_collision_error(key.clone(), node.span.clone()),
-                            ),
-                        })
-                    } else {
-                        acc.clone()
-                    }
-                }
-            }
+            Some(_) => acc.clone(),
             None => {
                 let acc1 = Rc::new(DagCollectAcc {
-                    seen: v2_rt::rc_map_insert(acc.seen.clone(), key.clone(), fp),
+                    seen: v2_rt::rc_map_insert(acc.seen.clone(), key.clone(), "".to_string()),
                     order: v2_rt::rc_list_push(acc.order.clone(), node.clone()),
                     collision_errors: acc.collision_errors.clone(),
                 });
