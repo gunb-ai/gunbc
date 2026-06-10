66a9edc8f3cc41005ec19d753a6bf3ee5cc67535
Brian Searls <briansearls1@gmail.com>
2026-06-10 07:20:43 +0000

    WIP: Fresh brand-identity binding_id implementation from #4587: BindingId/Typ

--- src/v2/stage0/src/v2_compiler_infer_env.rs
+++ src/v2/stage0/src/v2_compiler_infer_env.rs
@@ -165,7 +165,10 @@ pub fn authored_name(env: Rc<TypeEnv>, node: Rc<Node>) -> String {
 
 pub fn lookup_type_for(env: Rc<TypeEnv>, node: Rc<Node>) -> Option<Rc<Node>> {
     let visible = match node.ident.clone() {
-        Some(id) => lookup_type(env.clone(), id.clone()),
+        Some(id) => match lookup_type(env.clone(), id.clone()) {
+            Some(resolved) => Some(resolved),
+            None => lookup_type_by_name(env.clone(), authored_name(env.clone(), node.clone())),
+        },
         None => lookup_type_by_name(env.clone(), authored_name(env.clone(), node.clone())),
     };
     match visible {
