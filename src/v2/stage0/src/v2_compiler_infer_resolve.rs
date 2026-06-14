a19945bc6f31428e66dd4cd60808f980f74c93eb
Brian Searls <briansearls1@gmail.com>
2026-06-14 16:03:57 +0000

    WIP: G3: multi-hop alias field-access on v2 (MoneyMicros.count = MoneyAmount<

--- src/v2/stage0/src/v2_compiler_infer_resolve.rs
+++ src/v2/stage0/src/v2_compiler_infer_resolve.rs
@@ -340,6 +340,18 @@ pub fn resolve_node(n: Rc<Node>, env: Rc<TypeEnv>, module_name: String) -> Rc<No
 pub fn resolve_generic_use_decl(env: Rc<TypeEnv>, n: Rc<Node>) -> Rc<Node> {
     {
         let brand = authored_name(env.clone(), n.clone());
+        if std::env::var("DBG_RGUD").is_ok() {
+            let ltn = lookup_type_by_name(env.clone(), n.name.clone());
+            eprintln!(
+                "DBG rgud brand={} rawname={} children={} lookup_type_for_params={:?} ltbn={}",
+                brand,
+                n.name.clone(),
+                n.children.clone().len(),
+                lookup_type_for(env.clone(), n.clone()).map(|d| d.params.clone().len()),
+                ltn.map(|d| format!("Some(params={})", d.params.clone().len()))
+                    .unwrap_or("None".to_string()),
+            );
+        }
         match lookup_type_for(env.clone(), n.clone()) {
             Some(decl) => {
                 if ((decl.params.clone().len() as i64) > 0) {
