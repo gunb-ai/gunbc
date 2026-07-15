5d5473b43237a5f943fc010e566b1a256f9e2402
Brian Searls <briansrls@gunb.ai>
2026-07-15 21:41:05 +0000

    WIP: Namespace integration branch: collapse strip PRs #6603 #6600 #6607 #6610

--- src/v1/stage0/src/v1_compiler_infer.rs
+++ src/v1/stage0/src/v1_compiler_infer.rs
@@ -2949,7 +2949,8 @@ pub fn infer_expr(
                                     scope.module_name.clone(),
                                 )]),
                             })
-                        }
+                            }
+                        },
                     },
                 }
             }
@@ -12749,7 +12750,6 @@ pub fn local_binding_for_census_item(
         None => {
             if (item.body.clone() != None)
                 && ((item.params.clone().len() as i64) == 0)
-                && (item.inferred.clone() == None)
                 && (item.type_annotation.clone() != None)
             {
                 Some(Rc::new(TypeBinding {
