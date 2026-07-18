549ba22f63d34cebe3cb5411d6403729e0fb1ea4
Brian Searls <briansrls@gunb.ai>
2026-07-18 08:13:29 +0000

    WIP: Namespace integration branch: collapse strip PRs #6603 #6600 #6607 #6610

--- src/v1/stage0/src/v1_compiler_infer.rs
+++ src/v1/stage0/src/v1_compiler_infer.rs
@@ -12947,23 +12947,21 @@ pub fn corpus_disj_variant_name_counts(
     modules.clone().iter().cloned().fold(
         v1_rt::rc_empty_map::<String, i64>(),
         |counts: Rc<HashMap<String, i64>>, mod_: Rc<ResolvedModule>| {
-            module_items(mod_.module.clone())
-                .iter()
-                .cloned()
-                .fold(counts, |c: Rc<HashMap<String, i64>>, item: Rc<Node>| {
-                    match item.connective.clone() {
-                        Connective::Disj => item.children.clone().iter().cloned().fold(
-                            c,
-                            |c2: Rc<HashMap<String, i64>>, child: Rc<Node>| {
-                                disj_variant_name_count_inc(
-                                    c2,
-                                    authored_name_at(source_indices.clone(), child.clone()),
-                                )
-                            },
-                        ),
-                        _ => c,
-                    }
-                })
+            module_items(mod_.module.clone()).iter().cloned().fold(
+                counts,
+                |c: Rc<HashMap<String, i64>>, item: Rc<Node>| match item.connective.clone() {
+                    Connective::Disj => item.children.clone().iter().cloned().fold(
+                        c,
+                        |c2: Rc<HashMap<String, i64>>, child: Rc<Node>| {
+                            disj_variant_name_count_inc(
+                                c2,
+                                authored_name_at(source_indices.clone(), child.clone()),
+                            )
+                        },
+                    ),
+                    _ => c,
+                },
+            )
         },
     )
 }
@@ -12989,29 +12987,32 @@ pub fn symbol_index_insert_unique_disj_variant_aliases(
                         |a2: Rc<SymbolIndex>, child: Rc<Node>| {
                             let vname = authored_name_at(source_indices.clone(), child.clone());
                             match v1_rt::map_get(&counts, vname.clone()) {
-                                Some(1) => match v1_rt::map_get(&corpus_variant_counts, vname.clone())
-                                {
-                                    Some(1) => symbol_index_insert_decl(
-                                        a2.clone(),
-                                        v1_rt::concat(
-                                            v1_rt::concat(module_path.clone(), ".".to_string()),
-                                            vname.clone(),
+                                Some(1) => {
+                                    match v1_rt::map_get(&corpus_variant_counts, vname.clone()) {
+                                        Some(1) => symbol_index_insert_decl(
+                                            a2.clone(),
+                                            v1_rt::concat(
+                                                v1_rt::concat(module_path.clone(), ".".to_string()),
+                                                vname.clone(),
+                                            ),
+                                            Rc::new(TypeBinding {
+                                                name: vname.clone(),
+                                                resolved: child.clone(),
+                                                provenance: Rc::new(
+                                                    SubValueRelation::SubValueUnknown,
+                                                ),
+                                            }),
                                         ),
-                                        Rc::new(TypeBinding {
-                                            name: vname.clone(),
-                                            resolved: child.clone(),
-                                            provenance: Rc::new(SubValueRelation::SubValueUnknown),
-                                        }),
-                                    ),
-                                    _ => symbol_index_insert(
-                                        a2.clone(),
-                                        v1_rt::concat(
-                                            v1_rt::concat(module_path.clone(), ".".to_string()),
-                                            vname.clone(),
+                                        _ => symbol_index_insert(
+                                            a2.clone(),
+                                            v1_rt::concat(
+                                                v1_rt::concat(module_path.clone(), ".".to_string()),
+                                                vname.clone(),
+                                            ),
+                                            child.clone(),
                                         ),
-                                        child.clone(),
-                                    ),
-                                },
+                                    }
+                                }
                                 _ => a2.clone(),
                             }
                         },
