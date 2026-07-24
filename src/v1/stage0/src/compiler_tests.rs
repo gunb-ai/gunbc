7c60f6fbfee9195d9aa5bfd95ff4193960f6e75a
Brian Searls <briansearls1@gmail.com>
2026-07-24 20:12:39 +0000

    WIP: Continue emit import-closure to cargo-green

--- src/v1/stage0/src/compiler_tests.rs
+++ src/v1/stage0/src/compiler_tests.rs
@@ -869,10 +869,6 @@ mod compiler_tests {
             coerce_container_template(RenderTarget::Rust, "Set".into()),
             Some("BTreeSet<{0}>".to_string())
         );
-        assert_eq!(
-            coerce_container_template(RenderTarget::Rust, "PointwisePower".into()),
-            Some("BTreeSet<{0}>".to_string())
-        );
     }
 
     #[test]
@@ -902,10 +898,6 @@ mod compiler_tests {
             coerce_container_template(RenderTarget::Python, "Set".into()),
             Some("set[{0}]".to_string())
         );
-        assert_eq!(
-            coerce_container_template(RenderTarget::Python, "PointwisePower".into()),
-            Some("set[{0}]".to_string())
-        );
     }
 
     #[test]
@@ -933,10 +925,6 @@ mod compiler_tests {
         );
         assert_eq!(
             coerce_container_template(RenderTarget::Go, "Set".into()),
-            Some("map[{0}]struct{}".to_string())
-        );
-        assert_eq!(
-            coerce_container_template(RenderTarget::Go, "PointwisePower".into()),
             Some("map[{0}]struct{}".to_string())
         );
     }
