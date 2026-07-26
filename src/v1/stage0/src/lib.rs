b79c06386e762c54e633776233f40242283d5829
Brian Searls <briansearls1@gmail.com>
2026-07-26 03:08:14 +0000

    WIP: test umbrella dissolution

--- src/v1/stage0/src/lib.rs
+++ src/v1/stage0/src/lib.rs
@@ -16,6 +16,7 @@
 use im::{OrdSet as BTreeSet, Vector as Vec};
 
 pub mod cli_run;
+pub mod test_module_hygiene;
 pub mod coproduct_reflection;
 pub mod extdeps_cargo;
 pub mod extdeps_cargo_version;
@@ -74,7 +75,6 @@ pub mod std_syntax;
 pub mod std_termination;
 pub mod std_trait_derive_shape;
 pub mod std_types;
-pub mod test_module_hygiene;
 pub mod usv_pilot_v2_std_algebra;
 pub mod usv_pilot_v2_std_collection;
 pub mod usv_pilot_v2_std_node;
