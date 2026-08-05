ffdb829db99e745382eeae46e2c4af19c0320652
Brian Searls <briansearls1@gmail.com>
2026-08-05 10:06:10 +0000

    WIP: prose cleanup

--- src/v1/stage0/src/v1_tests_claim_v1_annotation_target_emission_test.rs
+++ src/v1/stage0/src/v1_tests_claim_v1_annotation_target_emission_test.rs
@@ -11,6 +11,7 @@ pub use crate::v1_compiler_parse::parse_with_table;
 pub use crate::v1_compiler_tokenize::tokenize_artifact;
 use crate::v1_rt;
 use crate::v1_rt::{VecCompat, VecJoin};
+pub use crate::v1_std_core::NewlineIndex;
 pub use crate::v1_std_core::{build_newline_index, empty_intern_table};
 use crate::NonEmptyBTreeSet;
 use crate::NonEmptyVec;
